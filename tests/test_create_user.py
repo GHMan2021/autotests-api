7f1cab232fd5df90a0fe55911a1300e53e7b11e0
GHMan2021 <statio2009@gmail.com>
2025-07-26 09:31:54 +0400

    Фикстуры Pytest. Практика написания автотестов с использованием фикстур

--- tests/test_create_user.py
+++ tests/test_create_user.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 # Импортируем функцию проверки статус-кода
 from tools.assertions.base import assert_status_code
@@ -12,22 +12,12 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users  # Добавили маркировку users
 @pytest.mark.regression  # Добавили маркировку regression
-def test_create_user():
-    # Инициализируем API-клиент для работы с пользователями
-    public_users_client = get_public_users_client()
-
-    # Формируем тело запроса на создание пользователя
+def test_create_user(public_users_client: PublicUsersClient):
     request = CreateUserRequestSchema()
-    # Отправляем запрос на создание пользователя
     response = public_users_client.create_user_api(request)
-    # Инициализируем модель ответа на основе полученного JSON в ответе
-    # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 
-    # Проверяем статус-код ответа
-    # assert response.status_code == HTTPStatus.OK, 'Некорректный статус-код ответа'
     assert_status_code(response.status_code, HTTPStatus.OK)
     assert_create_user_response(request, response_data)
 
-    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
     validate_json_schema(response.json(), response_data.model_json_schema())
