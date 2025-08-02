5b4d51ffd815a4f7fe2c8a2a15ab45dde13e6fb4
GHMan2021 <statio2009@gmail.com>
2025-08-02 08:25:51 +0400

    Практикуемся в написании API автотестов. Часть 3. Практика написания API автотестов для работы с заданиями. Получение списка заданий

--- tools/assertions/courses.py
+++ tools/assertions/courses.py
@@ -1,87 +1,87 @@
-from clients.errors_schema import InternalErrorResponseSchema
-from clients.exercises.exercises_schema import ExerciseSchema, CreateExerciseRequestSchema, \
-    CreateExerciseResponseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, UpdateExerciseResponseSchema
-from tools.assertions.base import assert_equal
-from tools.assertions.errors import assert_internal_error_response
+from clients.courses.courses_schema import CourseSchema, UpdateCourseRequestSchema, UpdateCourseResponseSchema, \
+    GetCoursesResponseSchema, CreateCourseResponseSchema, CreateCourseRequestSchema
+from tools.assertions.base import assert_equal, assert_length
+from tools.assertions.files import assert_file
+from tools.assertions.users import assert_user
 
 
-def assert_create_exercise_response(
-        request: CreateExerciseRequestSchema,
-        response: CreateExerciseResponseSchema
+def assert_update_course_response(
+        request: UpdateCourseRequestSchema,
+        response: UpdateCourseResponseSchema
 ):
     """
-    Проверяет, что ответ на создание задания соответствует запросу.
+    Проверяет, что ответ на обновление курса соответствует запросу.
 
-    :param request: Исходный запрос на создание задания.
-    :param response: Ответ API с данными задания.
+    :param request: Исходный запрос на обновление курса.
+    :param response: Ответ API с данными курса.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    assert_equal(response.exercise.title, request.title, "title")
-    assert_equal(response.exercise.course_id, request.course_id, "course_id")
-    assert_equal(response.exercise.max_score, request.max_score, "max_score")
-    assert_equal(response.exercise.min_score, request.min_score, "min_score")
-    assert_equal(response.exercise.order_index, request.order_index, "order_index")
-    assert_equal(response.exercise.description, request.description, "description")
-    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
 
 
-def assert_update_exercise_response(
-        request: UpdateExerciseRequestSchema,
-        response: UpdateExerciseResponseSchema
-):
+def assert_course(actual: CourseSchema, expected: CourseSchema):
     """
-    Проверяет, что ответ на обновление задания соответствует запросу.
+    Проверяет, что фактические данные курса соответствуют ожидаемым.
 
-    :param request: Исходный запрос на обновление задания.
-    :param response: Ответ API с данными задания.
-    :raises AssertionError: Если хотя бы одно поле не совпадает.
-    """
-    assert_equal(response.exercise.title, request.title, "title")
-    assert_equal(response.exercise.max_score, request.max_score, "max_score")
-    assert_equal(response.exercise.min_score, request.min_score, "min_score")
-    assert_equal(response.exercise.order_index, request.order_index, "order_index")
-    assert_equal(response.exercise.description, request.description, "description")
-    assert_equal(response.exercise.estimated_time, request.estimated_time, "estimated_time")
-
-
-def assert_exercise(actual: ExerciseSchema, expected: ExerciseSchema):
-    """
-    Проверяет, что фактические данные задания соответствуют ожидаемым.
-
-    :param actual: Фактические данные задания.
-    :param expected: Ожидаемые данные задания.
+    :param actual: Фактические данные курса.
+    :param expected: Ожидаемые данные курса.
     :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
     assert_equal(actual.id, expected.id, "id")
     assert_equal(actual.title, expected.title, "title")
-    assert_equal(actual.course_id, expected.course_id, "course_id")
     assert_equal(actual.max_score, expected.max_score, "max_score")
     assert_equal(actual.min_score, expected.min_score, "min_score")
-    assert_equal(actual.order_index, expected.order_index, "order_index")
     assert_equal(actual.description, expected.description, "description")
     assert_equal(actual.estimated_time, expected.estimated_time, "estimated_time")
 
+    assert_file(actual.preview_file, expected.preview_file)
+    assert_user(actual.created_by_user, expected.created_by_user)
 
-def assert_get_exercise_response(
-        get_exercise_response: GetExerciseResponseSchema,
-        create_exercise_response: CreateExerciseResponseSchema
+
+def assert_get_courses_response(
+        get_courses_response: GetCoursesResponseSchema,
+        create_course_responses: list[CreateCourseResponseSchema]
 ):
     """
-    Проверяет, что ответ на получение задания соответствует ответу на его создание.
+    Проверяет, что ответ на получение списка курсов соответствует ответам на их создание.
 
-    :param get_exercise_response: Ответ API при запросе данных задания.
-    :param create_exercise_response: Ответ API при создании задания.
-    :raises AssertionError: Если данные задания не совпадают.
+    :param get_courses_response: Ответ API при запросе списка курсов.
+    :param create_course_responses: Список API ответов при создании курсов.
+    :raises AssertionError: Если данные пользователя не совпадают.
     """
-    assert_exercise(get_exercise_response.exercise, create_exercise_response.exercise)
+    assert_length(get_courses_response.courses, create_course_responses, "courses")
+
+    for index, create_course_response in enumerate(create_course_responses):
+        assert_course(get_courses_response.courses[index], create_course_response.course)
 
 
-def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
+def assert_create_course_response(
+        request: CreateCourseRequestSchema,
+        response: CreateCourseResponseSchema
+):
     """
-    Функция для проверки ошибки, если задание не найдено на сервере.
+    Проверяет, что ответ на создание курса соответствует запросу.
 
-    :param actual: Фактический ответ.
-    :raises AssertionError: Если фактический ответ не соответствует ошибке "Exercise not found"
+    :param request: Исходный запрос на создание курса.
+    :param response: Ответ API с данными курса.
+    :raises AssertionError: Если хотя бы одно поле не совпадает.
     """
-    expected = InternalErrorResponseSchema(details="Exercise not found")
-    assert_internal_error_response(actual, expected)
+    assert_equal(response.course.title, request.title, "title")
+    assert_equal(response.course.max_score, request.max_score, "max_score")
+    assert_equal(response.course.min_score, request.min_score, "min_score")
+    assert_equal(response.course.description, request.description, "description")
+    assert_equal(response.course.estimated_time, request.estimated_time, "estimated_time")
+    assert_equal(
+        response.course.preview_file.id,
+        request.preview_file_id,
+        "preview_file_id"
+    )
+    assert_equal(
+        response.course.created_by_user.id,
+        request.created_by_user_id,
+        "created_by_user_id"
+    )
