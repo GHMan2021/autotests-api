1e874b24485c35d879918557de76d2f97ba44b55
GHMan2021 <statio2009@gmail.com>
2025-08-04 20:56:32 +0400

    Добавляем логирование для API автотестов. Практика работы с логированием

--- clients/public_http_builder.py
+++ clients/public_http_builder.py
@@ -1,6 +1,6 @@
 from httpx import Client
 
-from clients.event_hooks import curl_event_hook
+from clients.event_hooks import curl_event_hook, log_request_event_hook, log_response_event_hook
 from config import settings
 
 
@@ -14,6 +14,7 @@ def get_public_http_client() -> Client:
         timeout=settings.http_client.timeout,
         base_url=settings.http_client.client_url,
         event_hooks={
-            "request": [curl_event_hook]
+            "request": [curl_event_hook, log_request_event_hook],  # Логируем исходящие HTTP-запросы
+            "response": [log_response_event_hook]  # Логируем полученные HTTP-ответы
         }
     )
