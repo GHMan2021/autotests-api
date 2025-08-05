2a49433bd7df45815b3a8b9956f21e2dd84ae813
GHMan2021 <statio2009@gmail.com>
2025-08-05 17:57:43 +0400

    Added .gitignore

--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -1,4 +1,5 @@
 from pydantic import BaseModel, HttpUrl, Field
+from pydantic.v1 import FilePath
 
 from tools.fakers import fake
 
@@ -21,7 +22,7 @@ class CreateFileRequestSchema(BaseModel):
     filename: str = Field(default_factory=lambda: f"{fake.uuid4()}.png")
     # Директорию оставляем статичной, чтобы все тестовые файлы на сервере попадали в одну папку
     directory: str = Field(default="tests")
-    upload_file: str
+    upload_file: FilePath
 
 
 class CreateFileResponseSchema(BaseModel):
